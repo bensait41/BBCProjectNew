9b775bd0965865f3f3bb4840c5c8fc50ee7c8a39
bensait41 <bensait41@gmail.com>
2021-11-09 21:30:25 +0000

    NagativeLoginStepDefs something added

--- src/test/java/co/uk/bbc/pages/NegativeLoginPage.java
+++ src/test/java/co/uk/bbc/pages/NegativeLoginPage.java
@@ -4,19 +4,16 @@ import co.uk.bbc.utilities.Driver;
 import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.Locale;
 
-public class NaegativeLoginPage {
+public class NegativeLoginPage {
 
     private Faker faker;
 
-
-
-    public NaegativeLoginPage() {
+    public NegativeLoginPage() {
         PageFactory.initElements(Driver.get(), this);
     }
 
@@ -32,23 +29,12 @@ public class NaegativeLoginPage {
     @FindBy(xpath = "//button/span[text()='Sign in']")
     public WebElement singInButton;
 
-    public void login(String user, String pass) {
-
-        Faker faker = new Faker(new Locale("es"));
-
-        String username = faker.name().username(); // "josé luis.alonso"
-        String email = faker.internet().safeEmailAddress(); // "josé luis.alonso@example.com"
-
-
-    }
 
     @Test
-
     public void fakeEmail(){
 
        faker = new Faker(new Locale("es"));
 
-        String username = faker.name().username();
         String email = faker.internet().safeEmailAddress();
         System.out.println("email = " + email);
 
--- src/test/java/co/uk/bbc/stepDefinitions/NegativeLoginStepDefs.java
+++ src/test/java/co/uk/bbc/stepDefinitions/NegativeLoginStepDefs.java
@@ -1,4 +1,5 @@
 package co.uk.bbc.stepDefinitions;
 
 public class NegativeLoginStepDefs {
+    ///->>>>>
 }
