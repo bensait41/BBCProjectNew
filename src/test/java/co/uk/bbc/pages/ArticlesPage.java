29a491ff3c8353a0fc5f05c8aafa2b20f119777f
bensait41 <bensait41@gmail.com>
2021-11-11 12:03:19 +0000

    Completed Articles.feature file. Added all the step definitions to it and write down the necessary java code.

--- src/test/java/co/uk/bbc/pages/ArticlesPage.java
+++ src/test/java/co/uk/bbc/pages/ArticlesPage.java
@@ -1,6 +1,28 @@
 package co.uk.bbc.pages;
 
+import co.uk.bbc.utilities.BrowserUtils;
+import co.uk.bbc.utilities.ConfigurationReader;
+import co.uk.bbc.utilities.Driver;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class ArticlesPage {
+
+        public ArticlesPage(){PageFactory.initElements(Driver.get(),this);}
+
+        @FindBy(css = "input#orb-search-q")
+        public WebElement searchBox;
+
+        @FindBy(xpath = "//button[@class='se-searchbox__submit']")
+        public WebElement searchButton;
+
+        @FindBy(xpath = "//a[@class='ssrcss-bxvqns-PromoLink e1f5wbog0']")
+        public List<WebElement> sportsHeadings;
+
 
 
 }
