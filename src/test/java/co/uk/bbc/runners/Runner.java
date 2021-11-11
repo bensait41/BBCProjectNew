6cc8998a75d7bb2bae7d6644024429c52099c190
bensait41 <bensait41@gmail.com>
2021-11-11 20:01:00 +0000

    My Latest changes

--- src/test/java/co/uk/bbc/runners/Runner.java
+++ src/test/java/co/uk/bbc/runners/Runner.java
@@ -14,8 +14,8 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "co/uk/bbc/stepDefinitions",
-        dryRun =true,
-        tags = "@TestFeature3"
+        dryRun =false,
+        tags = "@TestFeature1"
 )
 public class Runner {
 }
